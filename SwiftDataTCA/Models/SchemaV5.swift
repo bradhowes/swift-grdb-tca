f456ab588d6d5ec1aaef0f72dc23d19aa0aa8749
Brad Howes <bradhowes@mac.com>
2024-07-18 10:06:02 +0300

    New schema without UUID.

--- SwiftDataTCA/Models/SchemaV5.swift
+++ SwiftDataTCA/Models/SchemaV5.swift
@@ -2,8 +2,8 @@ import Dependencies
 import Foundation
 import SwiftData
 
-enum SchemaV4: VersionedSchema {
-  static var versionIdentifier: Schema.Version { .init(4, 0, 0) }
+enum SchemaV5: VersionedSchema {
+  static var versionIdentifier: Schema.Version { .init(5, 0, 0) }
 
   static var models: [any PersistentModel.Type] {
     [
@@ -14,12 +14,10 @@ enum SchemaV4: VersionedSchema {
 
   @Model
   final class _Actor {
-    let id: UUID
     let name: String
     var movies: [_Movie]
 
-    init(id: UUID, name: String) {
-      self.id = id
+    init(name: String) {
       self.name = name
       self.movies = []
     }
@@ -27,14 +25,12 @@ enum SchemaV4: VersionedSchema {
 
   @Model
   final class _Movie {
-    let id: UUID
     let title: String
     var favorite: Bool = false
     var sortableTitle: String = ""
     @Relationship(inverse: \_Actor.movies) var actors: [_Actor]
 
-    init(id: UUID, title: String, favorite: Bool = false) {
-      self.id = id
+    init(title: String, favorite: Bool = false) {
       self.title = title
       self.favorite = favorite
       self.sortableTitle = SchemaV3._Movie.sortableTitle(title)
@@ -57,13 +53,12 @@ enum SchemaV4: VersionedSchema {
 
   static func makeMock(context: ModelContext) {
     @Dependency(\.withRandomNumberGenerator) var withRandomNumberGenerator
-    @Dependency(\.uuid) var uuid
     let index = withRandomNumberGenerator { generator in
       Int.random(in: 0..<mockData.count, using: &generator)
     }
 
     let entry = mockData[index]
-    let movie = _Movie(id: uuid(), title: entry.0)
+    let movie = _Movie(title: entry.0)
     context.insert(movie)
 
     for name in entry.1 {
@@ -74,19 +69,18 @@ enum SchemaV4: VersionedSchema {
   }
 
   static func fetchOrMakeActor(_ context: ModelContext, name: String) -> _Actor {
-    @Dependency(\.uuid) var uuid
     let predicate = #Predicate<_Actor> { $0.name == name }
     let fetchDescriptor = FetchDescriptor<_Actor>(predicate: predicate)
     if let actors = (try? context.fetch(fetchDescriptor)), !actors.isEmpty {
       return actors[0]
     }
 
-    let actor = _Actor(id: uuid(), name: name)
+    let actor = _Actor(name: name)
     context.insert(actor)
 
     return actor
   }
 }
 
-extension SchemaV4._Movie: Sendable {}
-extension SchemaV4._Actor: Sendable {}
+extension SchemaV5._Movie: Sendable {}
+extension SchemaV5._Actor: Sendable {}
