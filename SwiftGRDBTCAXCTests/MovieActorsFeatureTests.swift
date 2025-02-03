88b9c3a9b93354f285a176c8ba4fa6472b1709c3
Brad Howes <bradhowes@mac.com>
2025-02-03 21:50:39 +0100

    snapshot test

--- SwiftGRDBTCAXCTests/MovieActorsFeatureTests.swift
+++ SwiftGRDBTCAXCTests/MovieActorsFeatureTests.swift
@@ -91,10 +91,10 @@ final class MovieActorsFeatureTests: XCTestCase {
 
   @MainActor
   func testPreviewRenderWithButtons() throws {
-    guard !isOnGithub else {
-      _ = XCTSkip("Not working on Github")
-      return
-    }
+//    guard !isOnGithub else {
+//      _ = XCTSkip("Not working on Github")
+//      return
+//    }
     withSnapshotTesting(record: .failed) {
       let view = MovieActorsView.preview
       assertSnapshot(of: view, as: .image)
