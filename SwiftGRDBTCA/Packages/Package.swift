bf97f10afe7d9b2fcc992272048d367e232351ce
Brad Howes <bradhowes@mac.com>
2026-08-30 18:37:24 +0200

    Cleanup of packages.

--- SwiftGRDBTCA/Packages/Package.swift
+++ SwiftGRDBTCA/Packages/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,11 +11,11 @@ let package = Package(
     .library(name: "Models", targets: ["Models"])
   ],
   dependencies: [
-    .package(url: "https://github.com/pointfreeco/sqlite-data", from: "1.6.2"),
+    .package(url: "https://github.com/pointfreeco/sqlite-data", from: "1.11.0", traits: ["Tagged"]),
     .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.17.0"),
-    .package(url: "https://github.com/pointfreeco/swift-sharing", from: "2.9.1"),
-    .package(url: "https://github.com/pointfreeco/swift-structured-queries", from: "0.33.0"),
-    .package(url: "https://github.com/pointfreeco/swift-tagged", from: "0.10.0")
+    .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-sharing", from: "2.10.0"),
+    .package(url: "https://github.com/pointfreeco/swift-structured-queries", from: "0.38.0")
   ],
   targets: [
     .target(
@@ -23,17 +23,16 @@ let package = Package(
       dependencies: [
         .product(name: "SQLiteData", package: "sqlite-data", condition: nil),
         .product(name: "Dependencies", package: "swift-dependencies", condition: nil),
+        .product(name: "IdentifiedCollections", package: "swift-identified-collections", condition: nil),
         .product(name: "Sharing", package: "swift-sharing", condition: nil),
-        .product(name: "Tagged", package: "swift-tagged", condition: nil)
+        .product(name: "StructuredQueries", package: "swift-structured-queries", condition: nil)
       ]
     ),
     .testTarget(
       name: "ModelsTests",
       dependencies: [
         "Models",
-        .product(name: "Dependencies", package: "swift-dependencies", condition: nil),
-        .product(name: "Sharing", package: "swift-sharing", condition: nil),
-        .product(name: "Tagged", package: "swift-tagged", condition: nil)
+        .product(name: "Dependencies", package: "swift-dependencies", condition: nil)
       ]
     ),
   ]
